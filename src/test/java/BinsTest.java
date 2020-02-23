ea317215a40c81917556a58fd239b958e2ff56e2
hlin <Zxsaqw12@&$>
2020-02-23 18:53:03 -0500

    Testing test

--- src/test/java/BinsTest.java
+++ src/test/java/BinsTest.java
@@ -1,5 +1,16 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BinsTest {
 
+    @Test
+    public void incrementBin() {
+        Bins bin = new Bins(2, 12);
+
+    }
+
+    @Test
+    public void getBin() {
+    }
 }
