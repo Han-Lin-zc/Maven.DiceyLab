63364c71efae26a4c0877b643c8064dad10f6874
hlin <Zxsaqw12@&$>
2020-02-25 11:16:03 -0500

    Little changes to formatting

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,12 +1,8 @@
-
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.logging.Logger;
 
 public class DiceTest {
-
-    private static final Logger LOGGER = Logger.getLogger(Dice.class.getName());
 
     @Test
     public void tossAndSum() {
