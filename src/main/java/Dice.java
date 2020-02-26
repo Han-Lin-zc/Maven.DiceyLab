f7ff34a8ad434b93c1cee4360d081013b67e3057
hlin <Zxsaqw12@&$>
2020-02-26 14:10:35 -0500

    Use ThreadLocalRandom in Dice class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,26 +1,24 @@
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Dice {
 
     public int numberOfDice;
     private int sizes;
-    private Random random;
 
 
     public Dice(int numberOfDice, int sizes, long random) {
         this.numberOfDice = numberOfDice;
         this.sizes = sizes;
-        this.random = new Random(random);
     }
 
     public Dice(int numberOfDice, int sizes) {
         this.numberOfDice = numberOfDice;
         this.sizes = sizes;
-        this.random = new Random();
     }
 
     public int tossAndSum() {
         int sum = 0;
+        ThreadLocalRandom random = ThreadLocalRandom.current();
         for (int i = 0; i < numberOfDice; i++) {
             sum += random.nextInt(sizes) + 1;
         }
